f55c9d0b6e1f1f25a0d50ee42a1734d444777cf3
Chih Chiu <chih.chiu.19@gmail.com>
2021-07-05 16:11:11 -0700

    add grid

--- diagramlab/base.js
+++ diagramlab/base.js
@@ -30,6 +30,7 @@ class SVGRenderer {
     this.top = 0;
     this.width = 640;
     this.height = 480;
+    this.useGrid = true;
 
     this.elements = [];
   }
@@ -47,13 +48,28 @@ class SVGRenderer {
     svgElement = createSvgElement('svg');
     svgElement.setAttribute('viewBox', `${this.left} ${this.top} ${this.width} ${this.height}`);
 
+    // For grid, see: https://stackoverflow.com/questions/14208673/how-to-draw-grid-using-html5-and-canvas-or-svg
     const defsElement = createSvgElement('defs');
     defsElement.innerHTML = `
-            <marker id="endarrow" markerWidth="10" markerHeight="5" refX="10" refY="2.5" orient="auto" markerUnits="strokeWidth">
-                <polygon points="0 0, 10 2.5, 0 5" fill="${this.style.lineColor}" />
-            </marker>
-        `;
+      <marker id="endarrow" markerWidth="10" markerHeight="5" refX="10" refY="2.5" orient="auto" markerUnits="strokeWidth">
+        <polygon points="0 0, 10 2.5, 0 5" fill="${this.style.lineColor}" />
+      </marker>
+      <pattern id="smallGrid" width="10" height="10" patternUnits="userSpaceOnUse">
+        <path d="M 10 0 L 0 0 0 10" fill="none" stroke="gray" stroke-width="0.5"/>
+      </pattern>
+      <pattern id="grid" width="100" height="100" patternUnits="userSpaceOnUse">
+        <rect width="100" height="100" fill="url(#smallGrid)"/>
+        <path d="M 100 0 L 0 0 0 100" fill="none" stroke="gray" stroke-width="1"/>
+      </pattern>
+    `;
     svgElement.append(defsElement);
+    if (this.useGrid) {
+      const rect = createSvgElement('rect');
+      rect.setAttribute('width', '100%');
+      rect.setAttribute('height', '100%');
+      rect.setAttribute('fill', 'url(#grid)');
+      svgElement.append(rect);
+    }
 
     for (const element of this.elements) {
       svgElement.append(element);
