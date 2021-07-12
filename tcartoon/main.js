ad5e52a13c42dcd85c1478398533284def076edd
chih chiu <chih.chiu.19@gmail.com>
2021-07-11 17:02:25 -0700

    save

---
+++ tcartoon/main.js
@@ -0,0 +1,9 @@
+// References:
+//   - SVG color: https://stackoverflow.com/questions/22252472/how-to-change-the-color-of-an-svg-element#:~:text=You%20can't%20change%20the,or%20using%20inline.
+
+let playground;
+
+function onload() {
+  playground = document.querySelector('#playground');
+  playground.
+}
