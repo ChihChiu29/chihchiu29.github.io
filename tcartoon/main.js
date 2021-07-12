664ea84d6b56f1199625df2718d025521a42b08d
Chih Chiu <chih.chiu.19@gmail.com>
2021-07-12 00:17:52 -0700

    simple server

--- tcartoon/main.js
+++ tcartoon/main.js
@@ -1,9 +1,70 @@
 // References:
 //   - SVG color: https://stackoverflow.com/questions/22252472/how-to-change-the-color-of-an-svg-element#:~:text=You%20can't%20change%20the,or%20using%20inline.
+let channel = '';
+let viewers = {};
 
-let playground;
+const WIDTH = 600;
+const IMAGEY = 20;
 
-function onload() {
-  playground = document.querySelector('#playground');
-  playground.
+function updateViewers() {
+  // return;
+  // $.ajax({
+  //   dataType: "jsonp",
+  //   jsonp: "callback",
+  //   url: "",
+  //   success: function viewera(data) {
+  //     //seeing the result in the console
+  //     console.log(data);
+  //   }
+  // });
+  // return;
+  // $.ajax({
+  //   url: `https://tmi.twitch.tv/group/user/{channel}/chatters`,
+  //   dataType: 'jsonp',
+  //   jsonpCallback: 'photos',
+  //   jsonp: false,
+  // });
+  // `https://tmi.twitch.tv/group/user/{channel}/chatters`
+  $.get(`/viewers`).done((data) => {
+    // Example response: {"_links":{},"chatter_count":2,"chatters":{"broadcaster":[],"vips":[],"moderators":[],"staff":[],"admins":[],"global_mods":[],"viewers":["anotherttvviewer","mslenity"]}}
+    const newViewers = viewers;
+    const previousViewers = Object.keys(viewers);
+    for (const viewer of previousViewers) {
+      if (!newViewers.includes(viewers)) {
+        delete viewers[viewer];
+      }
+    }
+    for (const viewer of newViewers) {
+      if (!previousViewers.includes(viewer)) {
+        const initialX = Math.floor(Math.random() * WIDTH);
+        const groupElement = document.createElementNS('http://www.w3.org/2000/svg', 'g');
+        groupElement.innerHTML = `
+          <text font-size="24" text-anchor="start" x="5">hello</text>
+          <animateTransform attributeName="transform" attributeType="XML" type="translate" values="${initialX} ${IMAGEY}; ${WIDTH - 10} ${IMAGEY}; 0 ${IMAGEY}; ${initialX} ${IMAGEY}" dur="10s" repeatCount="indefinite" additive="sum" />
+          <g>
+            <image xlink:href="data/pussycat.svg"></image>
+            <animateTransform attributeName="transform" attributeType="XML" type="scale" values="1; 1.05; 1" repeatCount="indefinite" dur="0.5s" fill="freeze" additive="sum" />
+          </g>
+        `;
+        viewers[viewer] = groupElement;
+
+      }
+    }
+    for (const viewer of Object.keys(viewers)) {
+      if (newViewers.includes(viewer)) {
+
+      }
+    }
+
+
+    playground = document.querySelector('#playground');
+  });
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  channel = urlParams.get('channel');
+
+  setInterval(updateViewers, 2000);
+});
---
+++ tcartoon/server.js
@@ -0,0 +1,61 @@
+var got = require('got');
+var path = require('path');
+var express = require('express');
+var app = express();
+var fs = require('fs');
+
+var viewers = [];
+const port = 8080;
+
+function updateViewers() {
+  got('https://tmi.twitch.tv/group/user/supercatomeow/chatters', { json: true }).then(response => {
+    viewers = response.body.chatters.viewers;
+    console.log(`updated viewers: ${viewers}`);
+  }).catch(error => {
+    console.log(error);
+  });
+}
+
+setInterval(updateViewers, 10000);
+
+var dir = path.join(__dirname, '');
+
+var mime = {
+  html: 'text/html',
+  txt: 'text/plain',
+  css: 'text/css',
+  gif: 'image/gif',
+  jpg: 'image/jpeg',
+  png: 'image/png',
+  svg: 'image/svg+xml',
+  js: 'application/javascript'
+};
+
+app.get('*', function (req, res) {
+  console.log(req.path);
+  if (req.path === '/viewers') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(viewers));
+    return;
+  }
+
+  var file = path.join(dir, req.path.replace(/\/$/, '/index.html'));
+  if (file.indexOf(dir + path.sep) !== 0) {
+    return res.status(403).end('Forbidden');
+  }
+  var type = mime[path.extname(file).slice(1)] || 'text/plain';
+  var s = fs.createReadStream(file);
+  s.on('open', function () {
+    res.set('Content-Type', type);
+    s.pipe(res);
+  });
+  s.on('error', function () {
+    res.set('Content-Type', 'text/plain');
+    res.status(404).end('Not found');
+  });
+});
+
+app.listen(port, function () {
+  console.log(`Listening on http://localhost:${port}/`);
+});
