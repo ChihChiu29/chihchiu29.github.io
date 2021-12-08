107bad82fe136d953d96ff8511609dd4511e7171
Chih Chiu <chih.chiu.19@gmail.com>
2021-12-07 17:25:02 -0800

    remove send.cm code; not working

--- user_scripts/cloud_drive_download_helper.js
+++ user_scripts/cloud_drive_download_helper.js
@@ -9,9 +9,11 @@
 // @match        https://download.megaup.net/*
 // @match        https://megaup.net/*
 // @match        https://letsupload.io/*
-// @match        https://send.cm/*
 // @grant        GM_log
 // ==/UserScript==
+
+// Useful:
+//  - nextSibling
 
 (function() {
     GM_log('Welcome to Cloud Drive Download Helper!');
@@ -51,16 +53,6 @@
         runUntil(function() {
             for (const button of document.querySelectorAll('button')) {
                 if (button.innerText.toLowerCase().indexOf('download') >= 0) {
-                    GM_log('Start download!');
-                    button.click();
-                }
-            }
-        }, 5, 30);
-    } else if (hostname === 'send.cm') {
-        runUntil(function() {
-            for (const elem of document.querySelectorAll('script')) {
-                if (scrip.src.toLowerCase().indexOf('clipboard.min.js') >= 0) {
-                    const button = elem.nextSibling.nextSibling.nextSibling.nextSibling;
                     GM_log('Start download!');
                     button.click();
                 }
