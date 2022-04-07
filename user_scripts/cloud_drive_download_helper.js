c64343de0cbc0090b27efb0bc83fc2370d22dba6
Chih Chiu <chih.chiu.19@gmail.com>
2022-04-07 00:15:25 -0700

    1.11

--- user_scripts/cloud_drive_download_helper.js
+++ user_scripts/cloud_drive_download_helper.js
@@ -3,7 +3,7 @@
 // @author       Unknown
 // @description  NOT SET
 // @namespace    unknown.unknown@github.io
-// @version      1.10
+// @version      1.11
 // @run-at       document-end
 // @match        http://bluemediafiles.com/*
 // @match        https://mega.nz/*
